faaf632955ed49a76a17035f2b20dd5ab41bc322
Yousuff-GIT <yousuffrashid@gmail.com>
2025-06-17 10:37:51 +0530

    Added Exercise_4_Basic_Graphical_Primitives/java/lab/cahcet/edu/shapes/Shapes.java via uploader

---
+++ Exercise_4_Basic_Graphical_Primitives/java/lab/cahcet/edu/shapes/Shapes.java
@@ -0,0 +1,13 @@
+// Shapes.java - Main activity class for drawing graphical primitives
+package lab.cahcet.edu.shapes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Shapes extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shapes); // Sets the layout for this activity
+    }
+}
